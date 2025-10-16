38ff36772c58fb142fea80b0c293e790b8c27232
pierrick <pierrick.leleu@gmail.com>
2025-10-16 11:29:36 +0200

    feat: Harvesters build and use containers

--- main/role.harvester.js
+++ main/role.harvester.js
@@ -2,25 +2,43 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.store.getFreeCapacity() > 0) {
-            var source = Game.getObjectById(creep.memory.source_id);
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+        var source = Game.getObjectById(creep.memory.source_id);
+
+        // Find container
+        var container = source.pos.findInRange(FIND_STRUCTURES, 1, {
+            filter: s => s.structureType == STRUCTURE_CONTAINER
+        })[0];
+
+        // If harvester is full
+        if(creep.store.getFreeCapacity() == 0) {
+            // If container found, transfer energy
+            if(container) {
+                if(creep.transfer(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(container, {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+            // If no container, check for construction site
+            else {
+                var constructionSite = source.pos.findInRange(FIND_CONSTRUCTION_SITES, 1, {
+                    filter: s => s.structureType == STRUCTURE_CONTAINER
+                })[0];
+
+                // If construction site found, build it
+                if(constructionSite) {
+                    if(creep.build(constructionSite) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(constructionSite, {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+                }
+                // If no construction site, create one
+                else {
+                    creep.room.createConstructionSite(creep.pos, STRUCTURE_CONTAINER);
+                }
             }
         }
+        // If harvester is not full
         else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                            structure.structureType == STRUCTURE_SPAWN ||
-                            structure.structureType == STRUCTURE_TOWER) && 
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
+            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
     }
