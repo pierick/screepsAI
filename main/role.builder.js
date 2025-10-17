b6c35c75fda8a7226f608329efe3cb3e69e18632
pierrick <pierrick.leleu@gmail.com>
2025-10-17 15:31:17 +0200

    refactor: Centralize energy harvesting logic

--- main/role.builder.js
+++ main/role.builder.js
@@ -1,3 +1,5 @@
+var actionHarvest = require('action.harvest');
+
 var roleBuilder = {
 
     /** @param {Creep} creep **/
@@ -33,23 +35,7 @@ var roleBuilder = {
             }
         }
         else {
-            // Prioritize containers
-            var container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: s => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 0
-            });
-
-            if (container) {
-                if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(container, {visualizePathStyle: {stroke: '#ffaa00'}});
-                }
-            } 
-            // If no containers, harvest from sources
-            else {
-                var sources = creep.room.find(FIND_SOURCES);
-                if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-                }
-            }
+            actionHarvest.run(creep);
         }
     }
 };
