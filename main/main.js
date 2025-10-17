d07c54eba53c1c2f2e2b4dc25c1510b4616a002a
pierrick <pierrick.leleu@gmail.com>
2025-10-17 09:26:08 +0200

    feat: Add upgrader role
    Adds a new creep role for upgrading the room controller. This includes the role logic, spawning, and integration into the main loop.

--- main/main.js
+++ main/main.js
@@ -1,11 +1,39 @@
 var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function () {
 
     for(var name in Memory.creeps) {
         if(!Game.creeps[name]) {
             delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
         }
+    }
+
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+
+    if(harvesters.length < 2) {
+        var newName = 'Harvester' + Game.time;
+        console.log('Spawning new harvester: ' + newName);
+        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
+            {memory: {role: 'harvester'}});
+    }
+
+    if(upgraders.length < 1) {
+        var newName = 'Upgrader' + Game.time;
+        console.log('Spawning new upgrader: ' + newName);
+        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
+            {memory: {role: 'upgrader'}});
+    }
+
+    if(Game.spawns['Spawn1'].spawning) { 
+        var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+        Game.spawns['Spawn1'].room.visual.text(
+            '🛠️' + spawningCreep.memory.role,
+            Game.spawns['Spawn1'].pos.x + 1, 
+            Game.spawns['Spawn1'].pos.y, 
+            {align: 'left', opacity: 0.8});
     }
 
     for(var name in Game.creeps) {
@@ -13,20 +41,8 @@ module.exports.loop = function () {
         if(creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
         }
-    }
-
-    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    var sources = Game.spawns['Spawn1'].room.find(FIND_SOURCES);
-
-    if(harvesters.length < sources.length) {
-        var newName = 'Harvester' + Game.time;
-        
-        var assigned_creeps = _.map(harvesters, (c) => c.memory.source_id);
-        var available_source = _.find(sources, (s) => !_.contains(assigned_creeps, s.id));
-
-        if(available_source) {
-            Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
-                {memory: {role: 'harvester', source_id: available_source.id}});
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
         }
     }
 }
