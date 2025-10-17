c1a2072ff5953e90c7e07407f5a3bbc3ce099002
pierrick <pierrick.leleu@gmail.com>
2025-10-17 15:40:03 +0200

    feat: Upgrader now plans a road to the controller

--- main/role.upgrader.js
+++ main/role.upgrader.js
@@ -14,6 +14,15 @@ var roleUpgrader = {
             creep.say('⚡ upgrade');
         }
 
+        if (!creep.memory.roadPlanned) {
+            const path = creep.pos.findPathTo(creep.room.controller);
+            creep.memory.pathToController = path;
+            for (const pos of path) {
+                creep.room.createConstructionSite(pos.x, pos.y, STRUCTURE_ROAD);
+            }
+            creep.memory.roadPlanned = true;
+        }
+
         if(creep.memory.upgrading) {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
